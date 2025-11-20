a524af854333c75d991059f3f6a2f3ef54b1c282
arinasidr <sidorinaarina4@gmail.com>
2025-11-20 23:26:14 +0300

    debug rooms logic

--- semestrovka/server/rooms.py
+++ semestrovka/server/rooms.py
@@ -10,7 +10,7 @@ class Room:
 
     def add_player(self, client_socket, player_name):
         self.players[client_socket] = player_name
-        if len(self.players) == 2 or len(self.players) == 3:
+        if len(self.players) == 2:
             self.start_game()
 
     def remove_player(self, client_socket):
@@ -27,16 +27,18 @@ class RoomManager:
 
     def find_available_room(self):
         for room in self.rooms.values():
-            if room['status'] == 'waiting' and len(room['players']) < 3:
-                return room['room_id']
+            if len(room.players) < 3:
+                return room
         return None
 
     def create_new_room(self):
-        room_id = random.randint(50) #мб потом поменять на счетчик
-        room = Room(room_id)
-        self.rooms[room_id] = room
-        return room
-
+        while True:
+            room_id = random.randint(0, 50) #мб потом поменять на счетчик
+            if room_id not in self.rooms:
+                room = Room(room_id)
+                self.rooms[room_id] = room
+                return room
+        
     def add_client_to_room(self, client_socket, player_name):
         room = self.find_available_room()
         if not room:
@@ -52,18 +54,28 @@ class RoomManager:
                 if not room.players:
                     del self.rooms[room.room_id]
 
-    def broadcast_to_room(self, room_id, message, is_own = False):
-        if room_id in self.rooms:
-            for conn in self.rooms[room_id].players:
-                if conn != is_own:
-                    try:
-                        conn.sendall(pickle.dumps(message, protocol=pickle.HIGHEST_PROTOCOL))
-                    except:
-                        self.remove_client_from_room(conn)
+    def broadcast_to_room(self, room_id, message, is_own = None):
+        room = self.rooms.get(room_id)
+        if not room:
+            return
+        
+        for conn in list(room.players.keys()):
+            if conn is is_own:
+                continue
+            try:
+                conn.sendall(pickle.dumps(message, protocol=pickle.HIGHEST_PROTOCOL))
+            except:
+                self.remove_client_from_room(conn)
 
     def get_client_room(self, client_socket):
         for room in self.rooms.values():
             if client_socket in room.players:
                 return room.room_id
         return None
+    
+    def get_room_players(self, room_id):
+        room = self.rooms.get(room_id)
+        if not room:
+            return []
+        return list(room.players.values())
 
