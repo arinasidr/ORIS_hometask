4894ae59932d2b85eacc1142df48b4f4bdf6e9cc
arinasidr <sidorinaarina4@gmail.com>
2025-11-20 23:27:07 +0300

    dev new server with connection rooms.py

--- semestrovka/server/server.py
+++ semestrovka/server/server.py
@@ -1,78 +1,96 @@
 import socket
 import threading
 import pickle
-import random
 
-def serialize(msg):
-    return pickle.dumps(msg, protocol=pickle.HIGHEST_PROTOCOL)
+from rooms import RoomManager
 
-def deserialize(data):
-    return pickle.loads(data)
+class Server:
+    def __init__(self, host = '127.0.0.1', port = 12345):
+        self.host = host
+        self.port = port
+        self.server_socket = None
+        self.clients = {} # socket: player_name
+        self.rooms = RoomManager()
+        self.lock = threading.Lock()
 
-rooms = {}
-
-def find_available_room():
-    for room in rooms.values():
-        if room['status'] == 'waiting' and len(room['players']) < 3:
-            return room['room_id']
-    return None
-
-def create_new_room():
-    room_id = random.randint(50) #мб потом поменять на счетчик
-    room = {
-        "room_id": room_id,
-        "players": [],
-        "status": "waiting",
-        "state":None
-    }
-    rooms[room_id] = room
-    return room_id
-
-def add_client_to_room(client_socket, player_name):
-    available_room = find_available_room()
-    if not available_room:
-        room_id = create_new_room()
-    rooms[room_id]["players"].append((client_socket, player_name))
-
-
-def handle_client(conn, addr):
-    print(f"[ПОДКЛЮЧЕН] {addr}")
-    while True:
+    def start_server(self):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        with self.server_socket:
+            self.server_socket.bind((self.host, self.port))
+            self.server_socket.listen()
+            print(f"[СЕРВЕР ЗАПУЩЕН] {self.host} : {self.port}")
+            while True:
+                conn, addr = self.server_socket.accept()
+                print(f"Подключен клиент: {addr}")
+                client_thread = threading.Thread(target=self.handle_client, args=(conn, addr), daemon=True)
+                client_thread.start()
+    
+    def send_message(self, conn, message_dict):
         try:
-            data = conn.recv(1024)
-            if not data:
-                print(f"[ОТКЛЮЧЕНИЕ] {addr}")
-                break
+            pickle_mes = pickle.dumps(message_dict, protocol=pickle.HIGHEST_PROTOCOL)
+            conn.sendall(pickle_mes)
+        except ConnectionResetError:
+            self.rooms.remove_client_from_room(conn)
 
-            msg = deserialize(data)
-            print(f"[ПОЛУЧЕНО] {msg} от {addr}")
+    def remove_client(self, conn):
+        with self.lock:
+            if conn in self.clients:
+                player_name = self.clients[conn]
+                room_id = self.rooms.get_client_room(conn)
+                self.rooms.remove_client_from_room(conn)
+                del self.clients[conn]
+                print(f'[ОТКЛЮЧЕНИЕ] {player_name} из комнаты {room_id}')
+                if room_id in self.rooms.rooms:
+                    players_list = self.rooms.get_room_players(room_id)
 
-            response = {
-                "type": "server_ack",
-                "from_user": "server",
-                "room_id": None,
-                "data": "Сообщение получено"
-            }
-            conn.sendall(serialize(response))
+                    self.rooms.broadcast_to_room(room_id, {
+                        'type': 'room_update',
+                        'room_id': room_id,
+                        'data': players_list
+                    })
+            conn.close()
+
+    def handle_client(self, conn):
+        player_name = None
+        try:
+            while True:
+                data = conn.recv(2048)
+                if not data:
+                    break
+
+                msg_dict = pickle.loads(data)
+                msg_type = msg_dict.get('type').lower()
+
+                if msg_type == 'signup':
+                    player_name = msg_dict.get('data')
+                    with self.lock:
+                        self.clients[conn] = player_name
+                    room_id = self.rooms.add_client_to_room(conn, player_name)
+                    self.send_message(conn, {'type': 'join_room', 'room_id': room_id, 'data':room_id})
+                    players_list = self.rooms.get_room_players(room_id)
+                    self.rooms.broadcast_to_room(room_id, {'type': 'room_update', 'room_id': room_id, 'data': players_list})
+                
+                elif msg_type == 'chat':
+                    if player_name is None:
+                        continue
+                    room_id = self.rooms.get_client_room(conn)
+                    text = msg_dict.get('data')
+                    self.rooms.broadcast_to_room(room_id, {
+                        'type': 'chat',
+                        'from_user': player_name,
+                        'data': text
+                    }, conn)
+                
+                elif msg_type == 'disconnect':
+                    break
 
         except Exception as e:
-            print(f"[ОШИБКА] {addr}: {e}")
-            break
-    conn.close()
+             print('Ошибка в handle_client:', e)
 
-def accept_clients(server_socket):
-    while True:
-        conn, addr = server_socket.accept()
-        client_thread = threading.Thread(target= handle_client, args=(conn, addr),daemon=True)
-        client_thread.start()
-
-def start_server():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind(('127.0.0.1', 12345))
-    server_socket.listen()
-    print("[СЕРВЕР ЗАПУЩЕН]")
-    accept_clients(server_socket)
+        finally:
+            self.remove_client(conn)
 
 if __name__ == "__main__":
-    start_server()
+    server = Server()
+    server.start_server()
